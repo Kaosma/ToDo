0ee8ed8024267b32cd44afbd02e3dc9a291cbfd1
Erik Ugarte <erik-nu@hotmail.com>
2020-09-15 16:38:30 +0200

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Erik Ugarte on 2020-09-15.
+//  Copyright © 2020 Creative League. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
